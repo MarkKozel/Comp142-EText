f570e558077dec6446894630e508672ebaf34b92
Mark Kozel <Development@markkozel.net>
2021-11-21 16:09:05 -0800

    Fixed question component style issue

--- Comp142_EText/.vuepress/components/QuestionMC.vue
+++ Comp142_EText/.vuepress/components/QuestionMC.vue
@@ -1,44 +1,76 @@
 <template>
   <div class="question-mc">
-    <table style="width:100%">
+    <table class="questionTable" style="width: 100%">
+      <tr>
+        <!-- <td class="questionTd"> -->
+        <p class="header"><b>Quick Question:</b> Choose One</p>
+
+        <p class="question">{{ question }}</p>
+      </tr>
+
       <tr>
         <td>
-          <p class="header">
-            <b>Quick Question:</b>
-            <span class="question">{{ question }}</span>
-          </p>
-
-          <!-- <p class="question">{{ question }}</p> -->
-
           <label class="PossibleAnswers">
+            <input
+              type="radio"
+              name="radio"
+              value="A"
+              id="A"
+              v-model="picked"
+            />
             {{ AChoice }}
-            <input type="radio" name="radio" value="A" id="A" v-model="picked" />
             <span class="checkmark"></span>
           </label>
-
-          <label class="PossibleAnswers">
-            {{ BChoice }}
-            <input type="radio" name="radio" value="B" id="B" v-model="picked" />
-            <span class="checkmark"></span>
-          </label>
-
-          <label v-if="CChoice" class="PossibleAnswers">
-            {{ CChoice }}
-            <input type="radio" name="radio" value="C" id v-model="picked" />
-            <span class="checkmark"></span>
-          </label>
-
-          <label v-if="DChoice" class="PossibleAnswers">
-            {{ DChoice }}
-            <input type="radio" name="radio" value="D" id="D" v-model="picked" />
-            <span class="checkmark"></span>
-          </label>
-
-          <button v-if="picked" v-on:click="checkAnswer">Submit</button>
-          <p v-if="result">{{result}}</p>
         </td>
-        <td></td>
+        <td>
+          <label class="PossibleAnswers">
+            <input
+              type="radio"
+              name="radio"
+              value="B"
+              id="B"
+              v-model="picked"
+            />
+            {{ BChoice }}
+            <span class="checkmark"></span>
+          </label>
+        </td>
       </tr>
+
+      <tr>
+        <td>
+          <label v-if="CChoice" class="PossibleAnswers">
+            <input
+              type="radio"
+              name="radio"
+              value="C"
+              id="C"
+              v-model="picked"
+            />
+            {{ CChoice }}
+            <span class="checkmark"></span>
+          </label>
+        </td>
+        <td>
+          <label v-if="DChoice" class="PossibleAnswers">
+            <input
+              type="radio"
+              name="radio"
+              value="D"
+              id="D"
+              v-model="picked"
+            />
+            {{ DChoice }}
+            <span class="checkmark"></span>
+          </label>
+        </td>
+      </tr>
+
+      <button v-if="picked" v-on:click="checkAnswer">Submit</button>
+      <p v-if="result">{{ result }}</p>
+      <!-- </td> -->
+      <!-- <td></td> -->
+      <!-- </tr> -->
     </table>
   </div>
 </template>
@@ -48,44 +80,44 @@ module.exports = {
   props: {
     question: {
       type: String,
-      required: true
+      required: true,
     },
     AChoice: {
       type: String,
-      required: true
+      required: true,
     },
     BChoice: {
       type: String,
-      required: true
+      required: true,
     },
     CChoice: {
       type: String,
-      required: false
+      required: false,
     },
     DChoice: {
       type: String,
-      required: false
+      required: false,
     },
     answer: {
       type: String,
-      required: true
+      required: true,
     },
     rightAnswerFeedback: {
       type: String,
       required: false,
-      default: "Good job"
+      default: "Good job",
     },
     wrongAnswerFeedback: {
       type: String,
       required: false,
-      default: "Good question to review later"
-    }
+      default: "Good question to review later",
+    },
   },
-  data: function() {
+  data: function () {
     return {
       answered: false,
       picked: "",
-      result: ""
+      result: "",
     };
   },
   methods: {
@@ -95,22 +127,98 @@ module.exports = {
       } else {
         this.result = "Incorrect. " + this.wrongAnswerFeedback;
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
+button {
+  background-color: #0000ff;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  opacity: 0.9;
+  font-size: 16px;
+  font-family: Arial, sans-serif;
+}
 
-table {
+.PossibleAnswers {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default radio button */
+.PossibleAnswers input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom radio button */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #0000ff;
+  border-radius: 50%;
+}
+
+/* On mouse-over, add a grey background color */
+.PossibleAnswers:hover input ~ .checkmark {
+  background-color: #9999ff;
+}
+
+/* When the radio button is checked, add a blue background */
+.PossibleAnswers input:checked ~ .checkmark {
+  background-color: #9999ff;
+}
+
+/* Create the indicator (the dot/circle - hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the indicator (dot/circle) when checked */
+.PossibleAnswers input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the indicator (dot/circle) */
+.PossibleAnswers .checkmark:after {
+  top: 6px;
+  left: 6px;
+  width: 13px;
+  height: 13px;
+  border-radius: 80%;
+  background: white;
+}
+
+.questionTable {
   border-color: #0000ff;
   border-style: solid;
   border-width: 1px;
   border-spacing: 0px;
-    margin:0px;
-    padding:0px;
+  margin: 0px;
+  padding: 0px;
 }
-td {
+.questionTd {
   padding-top: 1px;
   padding-left: 10px;
   padding-bottom: 10px;
--- Comp142_EText/.vuepress/components/QuestionTF.vue
+++ Comp142_EText/.vuepress/components/QuestionTF.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="question-tf">
-    <table style="width: 100%">
+    <table class="questionTable" style="width: 100%">
       <tr>
+        <!-- <td> -->
+        <p class="header"><b>Quick Question:</b> True/False</p>
+
+        <p class="question">{{ question }}</p>
+<tr>
         <td>
-          <p class="header"><b>Quick Question:</b> True/False</p>
-
-          <p class="question">{{ question }}</p>
-
           <label class="PossibleAnswers">
             True
             <input
@@ -18,7 +19,8 @@
             />
             <span class="checkmark"></span>
           </label>
-
+        </td>
+        <td>
           <label class="PossibleAnswers">
             False
             <input
@@ -30,11 +32,12 @@
             />
             <span class="checkmark"></span>
           </label>
-
-          <button v-if="picked" v-on:click="checkAnswer">Submit</button>
-          <p v-if="result">{{ result }}</p>
         </td>
-        <td></td>
+</tr>
+        <button v-if="picked" v-on:click="checkAnswer">Submit</button>
+        <p v-if="result">{{ result }}</p>
+        <!-- </td> -->
+        <!-- <td></td> -->
       </tr>
     </table>
   </div>
@@ -82,15 +85,92 @@ module.exports = {
 </script>
 
 <style scoped>
-table {
-  border-color: #0000ff;
+button {
+  background-color: #0000ff;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  opacity: 0.9;
+  font-size: 16px;
+  font-family: Arial, sans-serif;
+}
+
+.PossibleAnswers {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default radio button */
+.PossibleAnswers input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom radio button */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #0000ff;
+  border-radius: 50%;
+}
+
+/* On mouse-over, add a grey background color */
+.PossibleAnswers:hover input ~ .checkmark {
+  background-color: #9999ff;
+}
+
+/* When the radio button is checked, add a blue background */
+.PossibleAnswers input:checked ~ .checkmark {
+  background-color: #9999ff;
+}
+
+/* Create the indicator (the dot/circle - hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the indicator (dot/circle) when checked */
+.PossibleAnswers input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the indicator (dot/circle) */
+.PossibleAnswers .checkmark:after {
+  top: 6px;
+  left: 6px;
+  width: 13px;
+  height: 13px;
+  border-radius: 80%;
+  background: white;
+}
+
+.questionTable {
+  border-color: #01013b;
   border-style: solid;
   border-width: 1px;
   border-spacing: 0px;
   margin: 0px;
   padding: 0px;
 }
-td {
+.questionTd {
   padding-top: 1px;
   padding-left: 10px;
   padding-bottom: 10px;
